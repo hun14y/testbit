2006aa780c60b7b50162b16f9419209aa5f8b446
ljs_931117 <ljs_931117@naver.com>
2021-06-16 16:02:12 +0900

    backUp

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -2,6 +2,8 @@ import time
 import pyupbit
 import datetime
 import requests
+import schedule
+from fbprophet import Prophet
 
 access = "mGtkK4oHuRzC87HhFq8DYXgZr5HgGzkbHrNJdO36"
 secret = "j4DprBWHAeLxqfQ33Y4ivhSOoAWBBJAdSqKCrtRa"
@@ -46,9 +48,31 @@ def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
+predicted_close_price = 0 # 당일 총가에 예측값을 저장한다.
+def predict_price(ticker):
+    """Prophet으로 당일 종가 가격 예측"""
+    global predicted_close_price
+    df = pyupbit.get_ohlcv(ticker, interval="minute60")
+    df = df.reset_index()
+    df['ds'] = df['index']
+    df['y'] = df['close']
+    data = df[['ds','y']]
+    model = Prophet()
+    model.fit(data)
+    future = model.make_future_dataframe(periods=24, freq='H')
+    forecast = model.predict(future)
+    closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=9)]
+    if len(closeDf) == 0:
+        closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=9)]
+    closeValue = closeDf['yhat'].values[0]
+    predicted_close_price = closeValue
+predict_price("KRW-BTC")
+schedule.every().hour.do(lambda: predict_price("KRW-BTC")) # 한시간마다 업데이트 해주도록한다.
+
+
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
-print("ㅁㄴㅇㅁㄴㅇ")
+print("머신러닝 시작입니다")
 # 시작 메세지 슬랙 전송
 post_message(myToken,"#-ai", "start")
 
@@ -57,12 +81,13 @@ while True:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-BTC")
         end_time = start_time + datetime.timedelta(days=1)
+        schedule.run_pending() #스케줄을 동작하고
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-BTC", 0.5)
-            ma15 = get_ma15("KRW-BTC")
             current_price = get_current_price("KRW-BTC")
-            if target_price < current_price and ma15 < current_price:
+            ma15 = get_ma15("KRW-BTC")
+            if target_price < current_price and ma15 < current_price and current_price < predicted_close_price: # 한시간마다 한번씩 종가의가격
                 krw = get_balance("KRW")
                 if krw > 5000:
                     buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
